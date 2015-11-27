64dedfa5bc8161f5876170aa5b5f6aab4b66fb1a
Mark Hinschberger <markhinschberger@gmail.com>
2015-11-27 11:26:52 +0800

    fixed rendering data bug

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,12 +1,12 @@
 Router.configure({
   layoutTemplate: 'layout'
 });
-
-Router.map(function(){
-  this.route('postsList', {path:'/'});
-
-  this.route('postPage', {
-    path: '/posts/:_id',
-    data: function(){return Posts.findOne(this.params._id);}
-  });
+Router.map(function() {
+  this.route('postsList', {path: '/'});
 });
+
+//   this.route('postPage', {
+//     path: '/posts/:_id',
+//     data: function(){return Posts.findOne(this.params._id);}
+//   });
+// });
